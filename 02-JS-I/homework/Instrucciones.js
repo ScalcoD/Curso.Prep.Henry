d9587c4ff57ca2224a6a28fb3a07d2497419fae5
Damian <damianscalco6@gmail.com>
2022-01-25 18:29:33 -0300

    Instrucciones

---
+++ 02-JS-I/homework/Instrucciones.js
@@ -0,0 +1,18 @@
+variables:
+Las variables son como una caja, en la cual almacenamos cosas, en el caso de Javascript, almacenamos elementos
+que se emplean para hacer referencia a otro valor que utilizaremos mas tarde 
+Strings:
+Los Strings son un tipo de dato de Javascript, siempre se utilizan entre comillas
+y son como hilos de caracteres.(palabras)
+Funciones:
+Las funciones son como Objetos invocables, los cuales podemos utilizar para calcular y resolver problemas o que 
+realizen cierta accion;
+Las funciones pueden incluir uno o varios argumentos entre parentesis, los cuales marcaran el valor de la funcion.
+Cuando queremos que las Funciones nos devuelvan algo, utilizamos la declaracion return, la cual detiene inmediatamente la funcion y devuelve lo especificado
+Declaraciones:
+En Javascript las instrucciones se llaman declaraciones, existen 3 tipos de declaraciones de variables
+var: para declarar una variable.
+let: declara una variable locas con ambito de bloque.
+const:Declara un nombre de constante de solo lectura y ambito de bloque.
+otro tipo de declaracion de variables es el if, un statement, el cual se usa para controlar el Flujo y comparar nuestra funcion.
+con If podremos verificar si los valores ingresados son True o false.
